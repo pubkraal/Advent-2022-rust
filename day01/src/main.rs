c85851e8b22615deac43d9bf109a3edb0d86e34b
John Kraal <john@johnkraal.com>
2022-12-04 20:50:59 +0100

    day01 improved

--- day01/src/main.rs
+++ day01/src/main.rs
@@ -5,11 +5,26 @@ use std::io::{BufRead, BufReader};
 fn main() {
     let f = File::open("../input/01.txt").unwrap();
     let file = BufReader::new(&f);
-    for (idx, line) in file.lines().enumerate() {
+
+    let mut elves = Vec::new();
+
+    let mut curelf = 0;
+
+    for (_, line) in file.lines().enumerate() {
         let x = line.unwrap();
         if x == "" {
-            println!("New elf on: {}", idx);
+            elves.push(curelf);
+            curelf = 0;
             continue;
         }
+        let val = x.parse::<i32>().unwrap();
+        curelf += val;
     }
+    elves.push(curelf);
+
+    elves.sort();
+    elves.reverse();
+
+    println!("p1: {}", elves[0]);
+    println!("p2: {}", elves[0] + elves[1] + elves[2]);
 }
